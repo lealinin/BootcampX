cb5ca7ce9591d68f91cbb97e3331a886977252fe
Lea Linin <lea.linin@gmail.com>
2020-01-06 22:21:08 +0000

    Create tables for cohorts and students

---
+++ migrations/students_cohorts.sql
@@ -0,0 +1,48 @@
+CREATE TABLE cohorts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  start_date DATE,
+  end_date DATE
+);
+
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255),
+  phone VARCHAR(32),
+  github VARCHAR(255),
+  start_date DATE,
+  end_date DATE,
+  cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
+);
+
+
+
+
+-- cohorts
+-- -------
+-- pk - id
+-- name
+-- start_date
+-- end_date
+
+-- students
+-- pk - id
+-- name
+-- age
+-- email
+-- phone
+-- github
+-- start_date
+-- end_date
+-- cohort_id
+
+
+
+
+CREATE TABLE famous_people (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  birthdate DATE
+);
