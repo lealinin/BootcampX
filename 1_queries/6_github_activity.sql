3af0541a548912958b569d979d245e5d9e180f98
Lea Linin <lea.linin@gmail.com>
2020-01-07 00:12:44 +0000

    Add query that selects all graduates without a linked Github account

---
+++ 1_queries/6_github_activity.sql
@@ -0,0 +1,7 @@
+-- Get all graduates without a linked Github account.
+-- Get their name, email, and phone.
+
+SELECT name, email, phone
+FROM students 
+WHERE github IS NULL
+AND end_date IS NOT NULL;
