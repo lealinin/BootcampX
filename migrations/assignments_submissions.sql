736b4f1956a22b2a5671521e83d04baa577d3348
Lea Linin <lea.linin@gmail.com>
2020-01-09 20:55:35 +0000

    Add queries that select students from cohorts and teachers that provided assistance in specific cohorts

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -31,3 +31,12 @@ CREATE TABLE assignment_submissions (
   duration INTEGER,
   submission_date DATE
 );
+
+CREATE TABLE assignments (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  content TEXT,
+  day INTEGER,
+  chapter INTEGER,
+  duration INTEGER
+);
